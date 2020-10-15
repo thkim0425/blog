926e9ff2d9ecef0a575b75108f5ae9d8a3f3e7e0
Taehoon Kim <thkim0425@hanmail.net>
2020-10-15 12:03:49 +0900

    front basic setting complete

--- client/src/store.js
+++ client/src/store.js
@@ -5,6 +5,7 @@ import { routerMiddleware } from "connected-react-router";
 
 import createRootReducer from "./redux/reducers/index";
 import rootSaga from "./redux/sagas";
+import { apply } from "redux-saga/effects";
 
 export const history = createBrowserHistory();
 
@@ -12,20 +13,17 @@ const sagaMiddleware = createSagaMiddleware();
 
 const initialState = {};
 
-const middlewares = {sagaMiddleware, routerMiddleware(history)}
-const devtools = window.EXTENSION
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
-const composeEnhancer = process.env.NODE_ENV === "production" ? compose:devtools || compose;
-
+const composeEnhancer =
+  process.env.NODE_ENV === "production" ? compose : devtools || compose;
 
 const store = createStore(
-    createRootReducer(history),
-    initialState,
-    
-)
+  createRootReducer(history),
+  initialState,
+  composeEnhancer(applyMiddleware(...middlewares))
+);
+sagaMiddleware.run(rootSaga);
 
-
-
-
-
-
+export default store;
